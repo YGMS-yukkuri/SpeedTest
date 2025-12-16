512dc9efd2bb9761171fb65ecceb044dc65951fc
yukkuri <reityan.sakura0104@gmail.com>
2025-12-16 16:55:38 +0900

    ボタンに色変更機能を追加し、描画機能を実装

--- C4/script.js
+++ C4/script.js
@@ -2,4 +2,22 @@ let color = "black";
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.strokeStyle = color;
+let drawing = false;
+
+
+function changeColor(Color) {
+    Color = color;
+}
+
+canvas.addEventListener("mousedown", drawing = true)
+canvas.addEventListener("mouseup", drawing = false)
+
+canvas.addEventListener("mousemove", (e) => {
+    if(!drawing)return;
+    x = e.clientX;
+    y = e.clientY;
+
+    
+    ctx.strokeStyle = color;
+
+})
