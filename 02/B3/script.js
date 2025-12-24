3d7fc52da1f7d24cbed425cce34c70df36ef47a0
yukkuri <reityan.sakura0104@gmail.com>
2025-12-25 00:31:46 +0900

    B3/ランダムパスワード

--- 02/B3/script.js
+++ 02/B3/script.js
@@ -1,5 +1,14 @@
 const input = document.querySelector("input");
 
+const chars = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&()-=^~|@;:,./+*"
 function gen() {
-    
+    let result = "";
+    for (let i = 0; i < 12; i++) {
+        const idx = Math.floor( Math.random() * chars.length )
+        //console.log(idx,result);
+        
+        result += chars[idx];
+    }
+    //console.log(result);
+    input.value = result;
 }
